fcfb97f04a6d7f2219100306d0e09ab7d14383d0
Hayley Wood <onlinecowork@gmail.com>
2021-06-07 04:13:23 +1000

    memory game update

---
+++ block-BJaaex/code/data2.js
@@ -0,0 +1,25 @@
+var cardsArray = [{
+    'name': 'shell',
+    'img': './img/blueshell.png'
+  }, {
+    'name': 'bobomb',
+    'img': './img/bobomb.png'
+  }, {
+    'name': 'mario',
+    'img': './img/mario.png'
+  }, {
+    'name': 'luigi',
+    'img': './img/luigi.png'
+  }, {
+    'name': '1up',
+    'img': './img/1up.png'
+  }, {
+    'name': 'bulletbill',
+    'img': './img/bulletbill.png'
+  }, {
+    'name': 'coin',
+    'img': './img/coin.png'
+  }, {
+    'name': 'goomba',
+    'img': './img/goomba.png'
+  }];
